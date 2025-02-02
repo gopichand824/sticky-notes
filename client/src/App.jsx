933bdd8868f9211997dd4267551d7712335bd7fe
gopichand <gopichand63063@gmail.com>
2025-02-02 08:40:59 +0530

    Initial commit: project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1>Sticky notes</h1>
+    </>
+  )
+}
+
+export default App
---
+++ server/app.js
@@ -0,0 +1,5 @@
+require('dotenv').config()
+
+
+
+console.log(process.env.MY_PASSWORD)
